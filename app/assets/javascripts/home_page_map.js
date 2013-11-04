28f83ce35ff2852868469007868e21988e03a01c
Drew Samsen <drew@weblogicnow.com>
2013-11-03 17:56:39 -0800

    Add 'remove' link with each new marker for easy removal

--- app/assets/javascripts/home_page_map.js
+++ app/assets/javascripts/home_page_map.js
@@ -1,7 +1,10 @@
 function initHomePageMap() {
 
-  // Enable the visual refresh
+  // Enable the visual refresh (newer google map UI)
   google.maps.visualRefresh = true;
+
+  // Global variable for storing new markers
+  markers = {}
 
   var mapOptions = homePageMapOptions();
   var container = $('#map-canvas')[0];
@@ -11,23 +14,45 @@ function initHomePageMap() {
   // functions can interact with it outside of initHomePageMap()
   map = new google.maps.Map(container, mapOptions);
 
+  //-----------------------------
+  // Event listeners for behavior
+  //-----------------------------
+
   // Create a new marker where the user clicks
   google.maps.event.addListener(map, 'click', function(event) {
     placeMarker(event.latLng);
-    showNewMarkerInList(event.latLng);
   });
+
+  //
+  // Removes markers
+  //
+  $('.locations').on("click", ".remove", function() {
+    var
+      $this = $(this),
+      markerId = $(this).attr('marker-id');
+    removeMarker(markerId);
+    $this.closest('li').remove();
+  })
+
+// Ends initHomePageMap()
 }
 
 
-//
-// Support functions:
-//
+//--------------------
+// Support functions
+// -------------------
 
+//
+// Start centered over Indianapolis
+//
 function startingLatLong() {
   var loc = new google.maps.LatLng(39.73, -86.27);
   return loc;
 }
 
+//
+// Default options for map init
+//
 function homePageMapOptions() {
   options = {
     center: startingLatLong(),
@@ -37,20 +62,62 @@ function homePageMapOptions() {
   return options;
 };
 
+//
+// Create new marker and process it
+//
 function placeMarker(location) {
   var marker = new google.maps.Marker({
       position: location,
       map: map
   });
+  storeNewMarker(marker);
+  showNewMarkerInList(marker);
 }
 
-function showNewMarkerInList(location) {
+//
+// Creates a reference to this new marker so we can
+// easily look it up by its __gm_id and manipulate
+// it later.
+//
+function storeNewMarker(marker) {
+  var id = marker.__gm_id;
+  markers[id] = marker;
+}
+
+//
+// Show, visually, the new markers coordinants and a
+// remove link.
+//
+function showNewMarkerInList(marker) {
   var
-    lat = location.lb,
-    lng = location.mb,
+    lat = marker.position.lb,
+
+    lng = marker.position.mb,
+
+    id = marker.__gm_id,
+
     toShow = "Lat: " + lat + ", Long: " + lng,
-    newItem = $('<li>').text(toShow);
+
+    removeButton = $('<a>')
+    .attr('href','javascript:void(0)')
+    .addClass('remove')
+    .attr('marker-id', id)
+    .text("Remove"),
+
+    newItem = $('<li>')
+    .text(toShow)
+    .append('<br />')
+    .append(removeButton);
+
   $('.locations').append(newItem);
+}
+
+//
+// Removes a marker based on it's __gm_id
+//
+function removeMarker(id) {
+  marker = markers[id];
+  marker.setMap(null);
 }
 
 
