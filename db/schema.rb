545dad4b1dd715aca1e9bc562737ac9e1a5b9e06
Drew Samsen <drew@weblogicnow.com>
2013-12-19 20:30:07 -0500

    Add lat/long to locations resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131220011626) do
+ActiveRecord::Schema.define(:version => 20131220012531) do
 
   create_table "locations", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.decimal  "lat",        :precision => 10, :scale => 6
+    t.decimal  "lng",        :precision => 10, :scale => 6
   end
 
   create_table "users", :force => true do |t|
